3fc66938cfce0b445a059f1846a112fad31702a3
Matsueda <matsueda0411@gmail.com>
2017-06-14 17:02:36 +0900

    AIControllerの更新

--- practice2/carrace/simplerace/e/AIController.java
+++ practice2/carrace/simplerace/e/AIController.java
@@ -3,163 +3,112 @@ import simplerace.*;
 
 public class AIController implements Controller, Constants {
 
+  double maxSpeed = 0;
+
   public void reset() {};
 
   public int control (SensorModel inputs) {
     int command=neutral;
-
-    if(inputs.getDistanceToNextWaypoint() > 0.05){
-      command = carMove(inputs,inputs.getAngleToNextWaypoint(), inputs.getDistanceToNextWaypoint());
+    if(inputs.getDistanceToNextWaypoint() >0.4){
+      maxSpeed = 9.0;
+    }else if(inputs.getDistanceToNextWaypoint() >0.2){
+      maxSpeed = 6.0;
+    }else if(isAhead(inputs.getAngleToNextWaypoint())){
+      maxSpeed=4;
+    }else{
+      maxSpeed = 2.3;
     }
-    else {
-      command = carMove(inputs,inputs.getAngleToNextNextWaypoint(), inputs.getDistanceToNextNextWaypoint());
-    }
+    command = carMove(inputs,inputs.getAngleToNextWaypoint(), inputs.getDistanceToNextWaypoint());
 
     //System.out.print(command);
 
     return command;
   }
   /**
-  進行方向を反転する
-  **/
-  private int reversalCommand(int command){
-    switch(command){
-      case 0:
-      command = forwardright;
-      break;
-      case 1 :
-      command = forward;
-      break;
-      case 2 :
-      command = forwardleft;
-      break;
-      case 3:
-      command = left;
-      break;
-      case 4 :
-      command = neutral;
-      break;
-      case 5 :
-      command = right;
-      break;
-      case 6 :
-      command = backwardright;
-      break;
-      case 7 :
-      command = backward;
-      break;
-      case 8 :
-      command = backwardleft;
-      break;
-    }
-    return command;
-  }
-  /**
   目標が前方にあるかを判定する。
   **/
-  private boolean fowardToNextPoint(double angle){
+  private boolean forwardToNextPoint(double angle){
+    return (angle > -Math.PI/4 && angle<=Math.PI/4);
+  }
+
+  private boolean isAhead(double angle){
     return (angle > -Math.PI/12 && angle<=Math.PI/12);
   }
   /**
   目標が後方にあるかを判定する。
   **/
   private boolean backwardToNextPoint(double angle){
-    return ((angle > Math.PI*11/12 && angle < Math.PI)|| (angle <= -Math.PI*11/12 && angle > -Math.PI));
+    return ((angle > Math.PI*3/4 && angle < Math.PI)|| (angle <= -Math.PI*3/4 && angle > -Math.PI));
   }
 
   private int carMove(SensorModel inputs,double angle, double distance){
     int command= neutral;
 
-    if(angle > 0 && angle <=Math.PI/2) {
-      command=forwardleft;
-    }
-    else if(angle < 0&&angle >= -Math.PI/2){
-      command=forwardright;
-    }
-    else if(angle < -Math.PI/2&&angle > -Math.PI){
-      command=backwardright;
-    }
-    else if(angle > Math.PI/2&&angle < Math.PI){
-      command=backwardleft;
-    }
-    else if(angle == 0){
-      command = forward;
-    }
-    else{
-      command = backward;
-    }
-
-
-    if(inputs.getSpeed()>7){ //速度の値が大きい時
-
-      if(angle > 0&& angle <= Math.PI/2.0) {
-        command=left;
-      }
-      else if(angle< 0&&angle >= -Math.PI/2.0){
-        command=right;
-      }
-      else if(angle < -Math.PI/2.0&&angle > -Math.PI){
-        command=backwardleft;
-      }
-      else if(angle > Math.PI/2.0&&angle < Math.PI){
-        command=backwardright;
-      }
-      else {
-        command=neutral;
-      }
-      if(inputs.getDistanceToNextWaypoint() < 0.05){
-        command = reversalCommand(command);
-      }
-
-    }
-    if(inputs.getSpeed()< -7){ //速度の値が小さい時
-      if(angle > 0&& angle <= Math.PI/2.0) {
-        command=forwardleft;
-      }
-      else if(angle< 0&&angle >= -Math.PI/2.0){
-        command=forwardright;
-      }
-      else if(angle < -Math.PI/2.0&&angle > -Math.PI){
-        command=left;
-      }
-      else if(angle > Math.PI/2.0&&angle < Math.PI){
-        command=right;
-      }
-      else {
-        command=neutral;
-      }
-      if(inputs.getDistanceToNextWaypoint() < 0.05){
-        command = reversalCommand(command);
-      }
-    }
-
-    if(distance< 0.08 ){
-      if(inputs.getSpeed() > 2 || inputs.getSpeed() < -2){
-        if(fowardToNextPoint(inputs.getAngleToNextWaypoint())) {
-          command=forward;
+    if(forwardToNextPoint(angle)){
+      if(angle<0){
+        if(inputs.getSpeed() < maxSpeed){
+          command = forwardright;
+        }else{
+          command = right;
         }
-        else if(backwardToNextPoint(inputs.getAngleToNextWaypoint())){
-          command=backward;
+      }else if(angle >0){
+        if(inputs.getSpeed() < maxSpeed){
+          command = forwardleft;
+        }else{
+          command = left;
         }
-        else if(inputs.getSpeed() < 0){
+      }else {
+        command = forward;
+      }
+
+    }else if(backwardToNextPoint(angle) && (distance <0.3 || inputs.getSpeed() <1)){
+      if(angle < 0){
+        command = backwardright;
+      }else{
+        command = backwardleft;
+      }
+    }else if(angle <0){
+      if(inputs.getSpeed() < 2){
+        command = forwardright;
+      }else{
+        command = backwardright;
+      }
+    }else{
+      if(inputs.getSpeed() < 2){
+        command = forwardleft;
+      }else{
+        command = backwardleft;
+      }
+    }
+
+    if(distance < 0.05){
+      if(!isAhead(angle) && inputs.getSpeed() > 1){
+        if(angle <0){
+          command = backwardleft;
+        }else{
+          command = backwardright;
+        }
+      }
+      if(!(backwardToNextPoint(inputs.getAngleToNextNextWaypoint()) && distance <0.3)){
+        if(inputs.getSpeed() > 2){
+          if(inputs.getAngleToNextNextWaypoint()<0){
+            command = right;
+          }else{
+            command = left;
+          }
+        }
+        if(forwardToNextPoint(inputs.getAngleToNextNextWaypoint())){
           command = forward;
-        }else {
-          command = backward;
         }
-
       }
     }
-
-    return command;
-  }
-
-  private int brake(double speed){
-    int command= neutral;
-    if(speed >= 0){
-      command = backward;
-    }
-    else {
-      command = forward;
+    if(distance<0.08){
+      if(isAhead(angle)){
+        if(isAhead(inputs.getAngleToNextNextWaypoint())){
+          command = forward;
+        }
+      }
+      return command;
     }
     return command;
   }
